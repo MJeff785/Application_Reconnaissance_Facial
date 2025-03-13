4c540c0a798f4278db9c1ee88952345a06dbc54e
MJeffin785 <maizeroi.gerson@gmail.com>
2025-03-13 11:33:15 -0300

    Import dependencies

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+#Importing the necessary libraries
 import sys
+import os
+import logging
+import cv2 
+import random
+import numpy as np
+from matplotlib import pyplot as pit 
 
-if sys.version_info < (3, 9) or sys.version_info >= (3, 10):
-    sys.exit("Error: This project requires Python 3.9.")
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Program started")
+    if sys.version_info < (3, 9) or sys.version_info >= (3, 10):
+        sys.exit("Error: This project requires Python 3.9.")
+
+if __name__ == "__main__":
+    main()
